327860d15934b720a976bac19465427043752c8d
extrude57 <gamemeup8@protonmail.com>
2020-06-11 17:31:46 -0700

    The project

---
+++ s.js
@@ -0,0 +1,14 @@
+
+function pp(){
+         document.cookie=p1;
+         var  p1 = 5;
+return p1;
+}
+
+
+    function docWrite(variable) {
+      new v = Date();
+      document.write(v);
+  }
+  
+  document.write('ff');
