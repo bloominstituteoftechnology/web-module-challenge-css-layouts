b014b2b08821d0bb272bfb70b0be479aa46944bc
josh jacques <jp.jacques@yahoo.com>
2020-12-11 01:04:26 -0600

    jquery and smooth scrolling in css

---
+++ script.js
@@ -0,0 +1,11 @@
+// $('a[href*="#"]').on('click', function (e) {
+//     e.preventDefault()
+  
+//     $('html, body').animate(
+//       {
+//         scrollTop: $($(this).attr('href')).offset().top,
+//       },
+//       1,
+//       'linear'
+//     )
+//   })
